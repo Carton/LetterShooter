753df8a32070f69fd367a31f5d627aa948dd4249
Carton He <cartonhe@gmail.com>
2023-04-15 22:52:51 +0800

    fixup

--- typing_game.py
+++ typing_game.py
@@ -7,9 +7,14 @@ from pygame.locals import *
 pygame.init()
 pygame.mixer.init()
 
+# 配置项
+HEALTH = 3
+
 # 定义颜色
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+
+# TODO: 显示一个大炮的图标来作为当前的剩余几条命的数量
 
 # TODO: 把所有尺寸改为相对值
 # 定义屏幕尺寸
@@ -30,14 +35,15 @@ explosion_image = pygame.transform.scale(explosion_image, (80, 80))
 
 shoot_sound = pygame.mixer.Sound("shoot.wav")
 explosion_sound = pygame.mixer.Sound("explosion.wav")
+wrong_key_sound = pygame.mixer.Sound("wrong_key.mp3")  # 添加错误按键音效
 
 # 设置字体
-# TODO: Use a comic like font
+# TODO：字体也修改一下，改为更容易辨认的字体，最好带有童趣的字体
 font = pygame.font.Font(None, 36)
 
 class Invader:
     def __init__(self):
-        # TODO: Try to generate normal word here
+        # NOTODO: Try to generate normal word here
         self.text = ''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for _ in range(random.randint(1, 3)))
         self.speed = random.uniform(1, 4)
         self.angle = random.uniform(-45, 45)
@@ -78,8 +84,8 @@ class Invader:
 def game_over(score):
     game_over_text = font.render(f'Game Over! Your score: {score}', True, BLACK)
     restart_text = font.render('Press R to restart the game', True, BLACK)
-    # 这里修改为清除所有Invaders，然后在原来背景上显示
-    screen.fill(WHITE)
+    # 在原来背景上显示
+
     screen.blit(game_over_text, (WIDTH / 2 - 100, HEIGHT / 2 - 50))
     screen.blit(restart_text, (WIDTH / 2 - 100, HEIGHT / 2))
     pygame.display.update()
@@ -91,6 +97,8 @@ def game_over(score):
                 sys.exit()
             elif event.type == KEYDOWN and event.key == K_r:
                 return
+
+
 
 def main():
     clock = pygame.time.Clock()
@@ -127,30 +135,42 @@ def main():
             if invader.update():
                 invaders_reached_ground += 1
                 invader.explode()
-                if invaders_reached_ground > 3:
+                if invaders_reached_ground >= HEALTH:
                     gameover = True
                     break
 
-        # TODO: 这里按键也需要修改一下
         for event in pygame.event.get():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 pygame.quit()
                 sys.exit()
             elif event.type == KEYDOWN:
                 typed_text += event.unicode
-                if typed_text == event.unicode:
-                    shoot_sound.play()
+                found_match = False
 
-                for invader in invaders:
-                    if invader.text == typed_text:
-                        invaders.remove(invader)
-                        score += len(invader.text)
-                        typed_text = ''
+                for i in range(2):
+                    for invader in invaders:
+                        if invader.text.startswith(typed_text):
+                            found_match = True
+                            if invader.text == typed_text:
+                                shoot_sound.play()
+                                invaders.remove(invader)
+                                score += len(invader.text)
+                                typed_text = ''
+                                break
+
+                    if i == 0 and not found_match:
+                        wrong_key_sound.play()  # 播放错误按键音效
+                        typed_text = event.unicode  # 重新开始输入
+                    else:
                         break
-            elif event.type == KEYUP:
-                typed_text = ''
 
-        clock.tick(30)
+        # 在屏幕上显示用户当前输入的按键内容
+        typed_text_render = font.render(typed_text, True, BLACK)
+        screen.blit(typed_text_render, (WIDTH / 2 - 50, HEIGHT * 4/5 + 50))
+
+
+
+        clock.tick(10)
         pygame.display.update()
 
     game_over(score)
